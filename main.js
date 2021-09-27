62211281d7520df74671ed5728977422655a7d84
Yousef Khalid <84613950+YousefKhalid-iq@users.noreply.github.com>
2021-09-27 19:56:20 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,54 +1,92 @@
-const button = document.querySelector(".container");
-const toggle = document.querySelector(".toggle-btn");
+const toggleOn = document.querySelector(".toggle-on-cont");
+const active = document.querySelector(".menu-container");
+const toggleOff = document.querySelector(".toggle-off-cont");
+const logo = document.querySelector(".logo-img");
 
-function  btnToggle() {
-  function toggleBasic() {
-		let x = document.getElementById("yearly-basic");
-		let y = document.getElementById("monthly-basic");
-
-		if (x.style.display === "block"){
-			x.style.display = "none";
-			y.style.display = "block";
-		} else {
-				x.style.display = "block";
-				y.style.display = "none";
-		}
-	}
-
-	function togglePro() {
-		let x = document.getElementById("yearly-pro");
-		let y = document.getElementById("monthly-pro");
-
-		if (x.style.display === "block"){
-			x.style.display = "none";
-			y.style.display = "block";
-		} else {
-				x.style.display = "block";
-				y.style.display = "none";
-		}
-	}
-
-	function toggleMaster() {
-		let x = document.getElementById("yearly-master");
-		let y = document.getElementById("monthly-master");
-
-		if (x.style.display === "block"){
-			x.style.display = "none";
-			y.style.display = "block";
-		} else {
-				x.style.display = "block";
-				y.style.display = "none";
-		}
-	}
-	toggleBasic();
-	togglePro();
-	toggleMaster();
-}
-
-button.addEventListener("click", function () {
-	toggle.classList.toggle('active');
+toggleOn.addEventListener("click", function () {
+	active.classList.toggle("press");
 });
 
+toggleOn.addEventListener("click", function () {
+	toggleOff.classList.toggle("press2");
+});
 
+toggleOn.addEventListener("click", function() {
+	toggleOn.classList.toggle("press3")
+});
 
+toggleOff.addEventListener("click", function() {
+	active.classList.toggle("press")
+});
 
+toggleOff.addEventListener("click", function() {
+	toggleOff.classList.toggle("press2")
+});
+
+toggleOff.addEventListener("click", function() {
+	toggleOn.classList.toggle("press3")
+});
+
+toggleOn.addEventListener("click", function() {
+	logo.classList.toggle("press3")
+})
+
+toggleOff.addEventListener("click", function() {
+	logo.classList.toggle("press3")
+})
+
+const tab1 = document.querySelector("#bookmark-div");
+const tab2 = document.querySelector("#intelligent-div");
+const tab3 = document.querySelector("#share-div");
+const main = document.getElementById("feature-btn-cont");
+const btn1 = document.getElementById("simple-btn");
+const btn2 = document.getElementById("speedy-btn");
+const btn3 = document.getElementById("easy-btn");
+
+	function toggleSimple() {
+
+		if (tab1.style.display === "none") {
+			tab1.style.display = "block";
+			tab2.style.display = "none";
+			tab3.style.display = "none";
+		} else {
+			tab1.style.display = "block"
+			tab2.style.display ="none";
+			tab3.style.display = "none";
+			btn1.addEventListener("click", function() {
+				tab1.classList.toggle("select-1")
+			});
+		}
+	}
+		
+	function toggleSpeedy() {
+		if (tab2.style.display === "none") {
+			tab2.style.display = "block";
+			tab1.style.display = "none";
+			tab3.style.display = "none";
+		} else {
+			tab2.style.display = "block";
+			tab1.style.display = "none";
+			tab3.style.display = "none";
+			btn2.addEventListener("click", function() {
+				tab2.classList.toggle("select-1")
+			});
+		}
+		
+	}
+		
+	function toggleEasy() {
+		if (tab3.style.display === "none") {
+			tab3.style.display = "block";
+			tab1.style.display = "none";
+			tab2.style.display = "none";
+		} else {
+			tab1.style.display = "none";
+			tab2.style.display = "none";
+			tab3.style.display = "block";
+			btn3.addEventListener("click", function() {
+				tab3.classList.toggle("select-1")
+			});
+		}
+	}	
+		
